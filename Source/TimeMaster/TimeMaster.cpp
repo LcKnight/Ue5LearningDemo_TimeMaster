365cacaffc6bd9039652c9c5795f02822067742d
LcKnight <jzh1199+GitHub.LcKnight@gmail.com>
2025-08-18 20:42:24 +0800

    first commit

---
+++ Source/TimeMaster/TimeMaster.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TimeMaster.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TimeMaster, "TimeMaster" );
