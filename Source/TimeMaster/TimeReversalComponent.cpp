5942dcbaffd4d1f2bafca748c56124b5583ec55e
LcKnight <jzh1199+GitHub.LcKnight@gmail.com>
2025-08-18 21:27:33 +0800

    sync

---
+++ Source/TimeMaster/TimeReversalComponent.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimeReversalComponent.h"
+
+// Sets default values for this component's properties
+UTimeReversalComponent::UTimeReversalComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	Owner = nullptr;
+	IsInit = false;
+	IsTimeReversing = false;
+	IsOutdated = false;
+	RecordTimeLength = 0.0f;
+	// ...
+}
+
+
+// Called when the game starts
+void UTimeReversalComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UTimeReversalComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UTimeReversalComponent::SetTimeReversing(bool TimeReversingState)
+{
+	IsTimeReversing = TimeReversingState;
+
+}
+
---
+++ Source/TimeMaster/TimeReversalComponent.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "Math/Vector.h"
+#include "Math/Rotator.h"
+
+
+#include "TimeReversalComponent.generated.h"
+
+
+struct TimeInfo {
+	FVector Location;
+	FRotator Rotation;
+	FVector LinearVelocity;
+	FVector AngularVelocity;
+	float DeltaTime;
+	TimeInfo(FVector _Location, FRotator _Rotation, FVector _LinearVelocity, FVector _AngularVelocity, float _DeltaTime) :
+		Location(_Location), Rotation(_Rotation), LinearVelocity(_LinearVelocity), AngularVelocity(_AngularVelocity){ }
+
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent),Blueprintable)
+class TIMEMASTER_API UTimeReversalComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTimeReversalComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+	AActor* Owner;
+	bool IsInit;
+	bool IsTimeReversing;
+	bool IsOutdated;
+	float RecordTimeLength;
+	TDoubleLinkedList<TimeInfo> TimeInfoFrames;
+
+	UFUNCTION(BlueprintCallable)
+	void SetTimeReversing(bool TimeReversingState);
+		
+};
